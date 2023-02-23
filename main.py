2db93559a1cd5c3ea3d4a1f619e4273c70c1ff25
sc4n1a471 <martin.terhes@icloud.com>
2023-02-23 22:01:47 +0100

    Login + search for the hardcoded license plate

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
+import time
+import pickle
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+from dotenv import load_dotenv
+# from requests import Session
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    load_dotenv()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # s = Session()
+
+    chromedriver = "/chromedriver"
+
+    option = webdriver.ChromeOptions()
+    option.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+
+    # driver = webdriver.Safari()
+    driver = webdriver.Chrome(options=option)
+
+    # try:
+    driver.get("https://magyarorszag.hu/jszp_szuf")
+    # cookies = pickle.load(open("cookies.pkl", "rb"))
+    # for cookie in cookies:
+    #     driver.add_cookie(cookie)
+
+    # time.sleep(7)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//form[@id="urn:eksz.gov.hu:1.0:azonositas:kau:1:uk:uidpwd"]')))
+    print("FOUND: Login methods")
+
+    driver.find_element(By.XPATH, '//form[@id="urn:eksz.gov.hu:1.0:azonositas:kau:1:uk:uidpwd"]').click()
+    print("CLICKED: Ugyfelkapus azonositas")
+    # form = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "urn:eksz.gov.hu:1.0:azonositas:kau:1:uk:uidpwd")))
+
+    # time.sleep(10)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//input[@id="fldUser"]')))
+    print("FOUND: username input field")
+
+    # driver.find_element(By.XPATH, '//label[text() = "Felhasználónév"]').click()
+    driver.find_element(By.XPATH, '//input[@id="fldUser"]').send_keys(os.getenv("USERNAME"))
+    print("FILLED: username")
+
+    driver.find_element(By.XPATH, '//input[@id="fldPass"]').send_keys(os.getenv("PASSWORD"))
+    print("FILLED: password filled")
+
+    time.sleep(1)
+
+    driver.find_element(By.XPATH, '//button[@name="submit"]').send_keys(Keys.ENTER)
+    print("CLICKED: login")
+
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//title[text() = "Jármű Szolgáltatási Platform"]')))
+    print("FOUND: Jármű Szolgáltatási Platform")
+
+    driver.switch_to.frame(1)
+
+    driver.find_element(By.XPATH, '//input[@id="input-rendszam"]').send_keys("AACQ435")
+    print("FILLED: license plate")
+
+    driver.find_element(By.XPATH, '//input[@id="input-rendszam"]').send_keys(Keys.ENTER)
+    print("Searching for license plate")
+
+    WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, '//title[contains(text() = "FORGALMI RENDSZ"])')))
+
+    driver.close()
+
+    asd = 0
