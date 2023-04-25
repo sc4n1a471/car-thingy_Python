c5faea22bfc01dbee4d2519d91d29e2fc152db19
sc4n1a471 <martin.terhes@icloud.com>
2023-04-25 22:44:37 +0200

    feat: added commands for headless chromedriver

--- app/data/settings.py
+++ app/data/settings.py
@@ -11,4 +11,14 @@ option.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave B
 # driver = webdriver.Safari()
 driver = webdriver.Chrome(options=option)
 
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+# options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
 credentials_location = "../../credentials.env"
