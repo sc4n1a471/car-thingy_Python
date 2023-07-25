1d063e9dedaa6958fe47a01a1e45b2a603613e9d
sc4n1a471 <martin.terhes@icloud.com>
2023-07-25 17:28:46 +0200

    fix: replaced some time.sleeps with webdriver.waits

--- application/src/get_data_methods/get_restrictions.py
+++ application/src/get_data_methods/get_restrictions.py
@@ -1,5 +1,7 @@
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 
 from application.data import settings
 from application.data.xpaths import XPATHS
@@ -9,9 +11,11 @@ def get_restrictions(car):
     Attributes:
         car -- car object
     """
+    # WebDriverWait(settings.driver, 5).until(ec.presence_of_element_located((By.XPATH, XPATHS.get("restrictions_tab"))))
     settings.driver.find_element(By.XPATH, XPATHS.get("restrictions_tab")).click()
     print("CLICKED: Restrictions")
-    time.sleep(settings.WAIT_TIME_TAB_CHANGE)
+
+    WebDriverWait(settings.driver, 1).until(ec.presence_of_element_located((By.XPATH, XPATHS.get("restrictions"))))
     restrictions = settings.driver.find_element(By.XPATH, XPATHS.get("restrictions"))
     restrictions_rows = restrictions.find_elements(By.TAG_NAME, "tr")
     for row in restrictions_rows:
