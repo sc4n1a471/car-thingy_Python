363982eb483c883e64e174a1380f4316a0fff9f5
sc4n1a471 <martin.terhes@icloud.com>
2023-08-12 23:53:31 +0200

    fix: increased wait times (#45)

--- application/src/get_data_methods/get_mileage.py
+++ application/src/get_data_methods/get_mileage.py
@@ -20,15 +20,21 @@ def get_mileage(car):
     settings.driver.find_element(By.XPATH, XPATHS.get("mileage_tab")).click()
     print("CLICKED: Mileage")
 
-    WebDriverWait(settings.driver, 1).until(ec.presence_of_element_located((By.XPATH, XPATHS.get("mileage"))))
-    mileage_tbody = settings.driver.find_element(By.XPATH, XPATHS.get("mileage"))
-    mileage_rows = mileage_tbody.find_elements(By.TAG_NAME, "tr")
+    counter = 0
+    while counter < 5:
+        WebDriverWait(settings.driver, 5).until(ec.presence_of_element_located((By.XPATH, XPATHS.get("mileage"))))
+        mileage_tbody = settings.driver.find_element(By.XPATH, XPATHS.get("mileage"))
+        mileage_rows = mileage_tbody.find_elements(By.TAG_NAME, "tr")
 
-    for row in mileage_rows:
-        tmp = row.text.split(" ")
-        if tmp != ['']:
-            print("FOUND: Mileage data")
-            mileage_num = ''.join(tmp[1:])
-            car.mileage.append(Mileage(tmp[0], int(mileage_num)))
-        else:
-            print("NOT FOUND: Mileage data")
+        for row in mileage_rows:
+            tmp = row.text.split(" ")
+            if tmp != ['']:
+                print("FOUND: Mileage data")
+                mileage_num = ''.join(tmp[1:])
+                car.mileage.append(Mileage(tmp[0], int(mileage_num)))
+                counter = 5
+            else:
+                print("NOT FOUND: Mileage data, searching again...")
+                counter += 1
+                time.sleep(0.25)
+                break
