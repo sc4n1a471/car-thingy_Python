9cf90e31308cabd8a52c032b92ab4beffc2689cf
Martin Terhes <martin.terhes@icloud.com>
2026-02-22 21:23:57 +0100

    feat: added prev query timestamp logging based waiting fix: on cancellation, it would continue sending messages

--- application/data/helpers.py
+++ application/data/helpers.py
@@ -25,6 +25,8 @@ async def send_to_client(sid: str, key: str, value: str | int | list | None, per
         percentage (int): Status percentage
         status (str, optional): pending/waiting/success/fail. Defaults to "pending".
     """
+    if car_requests[sid].status == "cancelled":
+        return
     logging.info(f"Sending to {sid}: {key} {value}, {percentage}, {status}")
 
     if percentage != -1:
@@ -45,13 +47,15 @@ async def send_to_client(sid: str, key: str, value: str | int | list | None, per
 
 # MARK: Send data
 def send(driver, cmd, params={}):
-    """
-    This is required to send CDP command to remote driver
+    """This is required to send CDP command to remote driver
     https://stackoverflow.com/questions/72121479/cdp-with-remote-webdriver-webdriver-object-has-no-attribute-execute-cdp-cmd
-    :param driver:
-    :param cmd: Mostly 'Network.<enable/setCookie/disable>'
-    :param params: Mostly '{}/cookie/{}'
-    :return:
+    Args:
+        driver (WebDriver): Driver object
+        cmd (str): Mostly 'Network.<enable/setCookie/disable>'
+        params (dict, optional): Mostly '{}/cookie/{}'. Defaults to {}.
+
+    Returns:
+        dict: Response
     """
     resource = "/session/%s/chromium/send_command_and_get_result" % driver.session_id
     url = driver.command_executor._url + resource
@@ -68,3 +72,30 @@ def check_auth(auth_key):
     req = requests.get(go_ip + "/auth", headers={"x-api-key": auth_key})
     if req.status_code != 200:
         raise ValueError("Invalid API key")
+
+
+# MARK: Get last query timestamp
+def get_query_timestemp(auth_key):
+    go_ip = os.getenv("GO_IP")
+    if go_ip is None:
+        raise ValueError("Environment variable GO_IP is not set")
+    req = requests.get(go_ip + "/query-timestamp", headers={"x-api-key": auth_key})
+    if req.status_code == 401:
+        raise ValueError("Invalid API key")
+    json_response = req.json()
+    if req.status_code != 200 or json_response is None or json_response["status"] != "success":
+        raise ValueError("Failed to get query timestamp")
+    return json_response["data"]
+
+
+def create_query_timestamp(auth_key, license_plate):
+    go_ip = os.getenv("GO_IP")
+    if go_ip is None:
+        raise ValueError("Environment variable GO_IP is not set")
+    req = requests.post(
+        go_ip + "/query-timestamp", headers={"x-api-key": auth_key}, json={"licensePlate": license_plate}
+    )
+    if req.status_code == 401:
+        raise ValueError("Invalid API key")
+    if req.status_code != 200:
+        raise ValueError("Failed to create query timestamp")
